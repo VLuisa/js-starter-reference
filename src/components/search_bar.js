0ee8eba533e425550c28ae972beed9a0225a34f4
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-07-14 13:43:30 -0400

    trying to merge versions

---
+++ src/components/search_bar.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchterm: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event) {
+    this.setState({ searchterm: event.target.value });
+    this.props.onSearchChange(this.state.searchterm);
+    console.log(event.target.value);
+  }
+  render() {
+    return (
+      <div id="searchbar">
+        <input onChange={this.onInputChange} value={this.state.searchterm} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
